84a666aa78d3d8be9b22a41bc4ffe3e5593153c9
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-01 19:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your code here
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
